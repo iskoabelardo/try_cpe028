e79f77f120a7e67bf6e5426f9a8147ba6be85123
iskoabelardo <qafgabelardo@tip.edu.ph>
2023-02-22 07:55:16 +0000

    last commit

---
+++ try.py
@@ -0,0 +1,11 @@
+class Try:
+    
+    def __init__(self, name, position, rank):
+        self.name = name
+        self.position = position
+        self.rank = rank
+
+isko = Try("Angelo","Midlaner","Divine")
+
+print("Name:" + isko.name)
+
